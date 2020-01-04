a3faef794349af6433b7613ea74c18687c4551b4
varaprasadp <vara199715@gmail.com>
2020-01-04 20:49:03 +0530

    intial second application

---
+++ yyy/src/main/java/com/dbs/yyy/YyyApplication.java
@@ -0,0 +1,13 @@
+package com.dbs.yyy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YyyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YyyApplication.class, args);
+	}
+
+}
---
+++ yyy/src/test/java/com/dbs/yyy/YyyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbs.yyy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YyyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
